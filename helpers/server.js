7c7d11d1a723aa1e91af91db83cd7fb992a94b4f
Johan Satgé <johansatge@users.noreply.github.com>
2020-04-19 02:25:29 +0200

    Server refactoring

---
+++ helpers/server.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const { log } = require('./log.js')
+const { getAvailableModes, getDefaultMode } = require('./modes.js')
+const {
+  registerSseClient,
+  sendSseEventToClient,
+  sendSseEventToClients,
+  startSsePing,
+} = require('./sse.js')
+const path = require('path')
+const pkg = require('../package.json')
+const { getState, setState } = require('./state.js')
+const { requestShutdown, isDryRun } = require('./system.js')
+
+const m = {}
+module.exports = m
+
+m.startServer = function({ port }) {
+  const app = express()
+  app.set('view engine', 'ejs')
+  app.set('views', path.join(__dirname, '..'))
+
+  app.get('/', responseHome)
+  app.get('/shutdown', responseShutdown)
+  app.get('/setmode/:mode', responseSetMode)
+  app.get('/setstate', responseSetState)
+  app.get('/sse', responseSse)
+  app.get('/viewstate', responseViewState)
+  app.use('/static', express.static(path.join(__dirname, '../static')))
+
+  app.listen(port, function () {
+    log(`Server started on http://localhost:${port} (${isDryRun() ? 'DRY' : 'LIVE'})`)
+    startSsePing()
+  })
+}
+
+function responseHome(request, response) {
+  getAvailableModes()
+    .then((modes) => {
+      response.status(200).render('index', { viewTitle: pkg.name, modes })
+    })
+    .catch((error) => {
+      response.status(500).send(`Could not get modes (${error.message})`)
+    })
+}
+
+function responseSetMode(request, response) {
+  getState()
+    .then((state) => {
+      return setState({ currentMode: request.params.mode, stateData: state.data })
+    })
+    .then((state) => {
+      sendSseEventToClients({ eventName: 'stateUpdate', data: state })
+      response.status(200).send('ok')
+    })
+}
+
+function responseSetState(request, response) {
+  const mode = request.query.mode
+  const data = JSON.parse(request.query.data)
+  getState()
+    .then((state) => {
+      state.data[mode] = data
+      return setState({ currentMode: state.currentMode, stateData: state.data })
+    })
+    .then((state) => {
+      sendSseEventToClients({ eventName: 'stateUpdate', data: state })
+      response.status(200).send('ok')
+    })
+}
+
+function responseShutdown(request, response) {
+  requestShutdown()
+  response.status(200).send('ok')
+}
+
+function responseViewState(request, response) {
+  getState().then((state) => {
+    response.status(200).json(state)
+  })
+}
+
+function responseSse(request, response) {
+  const clientId = registerSseClient({ request, response })
+  getState().then((state) => {
+    sendSseEventToClient({ clientId, eventName: 'stateUpdate', data: state })
+  })
+}
+
