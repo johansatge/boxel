6d2df78cad2b9d6b1960b9c14834f3f83878b305
Johan Satgé <johansatge@users.noreply.github.com>
2020-05-03 15:55:02 +0200

    Fix sync script

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Syncing files to pi"
-rsync app pi@192.168.1.6:/home/pi/boxel-app \
+rsync app/ pi@192.168.1.6:/home/pi/boxel-app \
   --delete \
   --recursive \
   --exclude=.DS_Store \
